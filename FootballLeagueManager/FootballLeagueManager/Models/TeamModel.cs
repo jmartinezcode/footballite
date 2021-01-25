2d76653b0e8cca8a0b2028d7ce0855b5e5ccf91a
sanchezoleagustavo-dev <71840344+sanchezoleagustavo-dev@users.noreply.github.com>
2021-01-25 16:29:41 -0600

    Create TeamModel.cs

---
+++ FootballLeagueManager/FootballLeagueManager/Models/TeamModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FootballLeagueManager.Models
+{
+    public class TeamModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey("League")]
+        public int LeagueId { get; set; }
+        [Required]
+        [DisplayName("Team Name")]
+        public string Name { get; set; }
+    }
+}
