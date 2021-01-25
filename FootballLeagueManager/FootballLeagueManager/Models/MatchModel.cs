ca27b9662a7e6c4b27e8b105fd926f03bf793dd1
Abe Sanchez <sanchez95san@gmail.com>
2021-01-24 21:35:30 -0600

    Create MatchModel.cs
    New model created, will add-migration when league and team tables have been added

---
+++ FootballLeagueManager/FootballLeagueManager/Models/MatchModel.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FootballLeagueManager.Models
+{
+    public class MatchModel
+    {
+        [Key]
+        public int Id { get; set; }
+        [ForeignKey("Team")]
+        public int TeamOneId { get; set; }
+        [ForeignKey("Team")]
+        public int TeamTwoId { get; set; }
+        public int ScoreForTeamOne { get; set; }
+        public int ScoreForTeamTwo { get; set; }
+        [Required]
+        public bool IsFinished { get; set; }
+        [Required]
+        public DateTime DateOfMatch { get; set; }
+    }
+}
