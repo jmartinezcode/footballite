8717dac5ffff739ee68f98121370d632047cd45a
Jacob M <sinii.jm@gmail.com>
2021-01-28 20:46:19 -0600

    Adding Create Logic for League

--- FootballLeagueManager/FootballLeagueManager/Controllers/LeagueController.cs
+++ FootballLeagueManager/FootballLeagueManager/Controllers/LeagueController.cs
@@ -54,15 +54,46 @@ namespace FootballLeagueManager.Controllers
         // POST: LeagueController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(League league)
+        public IActionResult Create(LeagueTeamMatchViewModel viewModel)
         {
             if (ModelState.IsValid) 
             {
+                var league = viewModel.League;
+                var teams = viewModel.Teams;
+                var matches = viewModel.Matches;
+
                 _context.Leagues.Add(league);
+                // # of teams need to be established
+                foreach (var team in teams)
+                {
+                    _context.Teams.Add(team);
+                }
+                // matches need to be created round robin for n-1 weeks (8 Team Example):
+                // Week #1: 1v2; 3v4; 5v6; 7v8;
+                // Week #2: 4v5; 7v1; 3v2; 6v8; 
+                // Week #3: 2v4; 1v3; 5v8; 6v7;
+                // Week #4: 1v4; 7v5; 6v2; 8v3;
+                // Week #5: 5v1; 4v7; 8v2; 3v6;
+                // Week #6: 8v1; 2v5; 4v6; 3v7;
+                // Week #7: 8v4; 6v1; 2v7; 5v3;
+                // 1: 3A,4H; 2: 3A,4H; 3: 4A,3H; 4: 3A,4H; 5: 4A,3H; 6: 4A,3H; 7: 3A,4H; 8: 4A,3H 
+                // Teams should have (as close as possible) even number of home and away games
+                // For odd number of Team leagues (7 Team Example):
+                // W1: 1v2; 3v4; 5v6; 7Bye
+                // W2: 4v5; 7v1; 3v2; 6Bye
+                // W3: 2v4; 3v7; 6v1; 5Bye
+                // W4: 2v7; 1v5; 3v6; 4Bye
+                // W5: 
+
+                foreach (var match in matches)
+                {
+                    _context.Matches.Add(match);
+                }
+                
                 _context.SaveChanges();
                 return RedirectToAction(nameof(Index));
             }
-            return View(league);
+            return View(viewModel);
         }
 
         // GET: LeagueController/Edit/5
